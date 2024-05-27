5bf180ded55a97ecf951de86d90c047c088951b2
Dmitriy <rsp.fdrf@yandex.ru>
2024-05-27 21:10:09 +0500

    adds comments for functions

--- plugins/vuex.js
+++ plugins/vuex.js
@@ -20,32 +20,43 @@ const store = createStore({
         };
     },
     mutations: {
+        // перезаписывает посты для главной страницы 
         changePage (state, response) {
             state.posts = response;
         },
+        // переключает значение isLoading, для отображения el-skeleton
         toggleIsLoading(state) {
             state.isLoading =!state.isLoading;
         },
+        // переключает значение IsUserSelected, для отображения кнопок переключения страниц
         toggleIsUserSelected(state) {
             state.isUserSelected =!state.isUserSelected
         },
+        // перезаписывает номер страницы с постами
         curPage(state, page){
             state.curPage = page;
         },
+        // перезаписывает данные поста на странице [id].vue
         changePost (state, response) {
             state.post = response;
         },
+        // перезаписывает данные пользователей для вывода в el-select
         changeOptions (state, response) {
             state.options = response.map(({ username, id }) => ({ username, id }));
         },
+        // переключает значение isEditMode, 
+        // для отображения полей при изменении данных поста на странице [id].vue
         toggleEdit (state) {
             state.isEditMode = !state.isEditMode;
         },
+        // записывает данные поста для вывода в el-input, дальнейшего редактирования и отправки запроса на изменени на сервер
         postEdited (state) {
             state.postEdited = { ...state.post }
         },
     },
     actions: {
+
+        // Загружает с сервера посты для страницы
         async changePage({ commit }, page) {
             commit('toggleIsLoading')
             try {
@@ -63,6 +74,8 @@ const store = createStore({
                 commit('toggleIsLoading')
             }
         },
+
+        // Загружает с сервера username и id пользователей 
         async fetchUsers({commit}) {
             try {
                 const response = await $fetch(`${API_BASE_URL}users`)
@@ -71,6 +84,8 @@ const store = createStore({
                 alert('Erorr')
             }
         },
+
+        // Загружает все посты выбранного пользователя
         async filterUsers({ commit }, userId) {
             try {
                 commit('toggleIsLoading')
@@ -83,6 +98,8 @@ const store = createStore({
                 commit('toggleIsLoading')
             }
         },
+
+        // Загружает данные выбранного поста для страницы [id].vue
         async fetchPost({ state, commit }, postNum) {
             try {
                 if(state.isEditMode) {
@@ -102,6 +119,8 @@ const store = createStore({
                 }
             }
         },
+
+        // Отправляет запрос на изменение title и body выбранного поста на странице [id].vue
         async updatePost({ state, commit }, postNum) {
             try {
                 await $fetch(`${API_BASE_URL}posts/${postNum}`, {
@@ -118,6 +137,7 @@ const store = createStore({
                     console.log('updatePost error')
                 }
         },
+        // данная ручка нужна для скрытия/показа кнопок переключения страниц, загрузки постов выбранного пользователя
         handleFilter({ state,commit, dispatch }, userId) {
             if (userId) {
                 if(!state.isUserSelected) {
